224239791b940fbab59e534ecc4d0600e74e9fcd
AlexRozh <alexrozh1988@gmail.com>
2021-07-24 23:46:01 +0300

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Alexander Rozhdestvenskiy on 24.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
